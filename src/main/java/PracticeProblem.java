d93a5e0e1930bbcba5e5d04e9045f8e92c899be2
toby-tan-ics4u1-02-25 <toby.tan@student.tdsb.on.ca>
2025-06-07 21:30:49 +0000

    ddodne

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,61 @@
 public class PracticeProblem {
+    public static void main(String[] args) {
 
-	public static void main(String args[]) {
+    }
+}
 
-	}
+class Rectangle {
+    private double length;
+    private double width;
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+    public Rectangle() {
+        this.length = 4;
+        this.width = 8;
+    }
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+    public Rectangle(double length) {
+        this.length = length;
+        this.width = 8;
+    }
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+    public double getLength() {
+        return length;
+    }
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+    public double getWidth() {
+        return width;
+    }
+}
 
+class Chair {
+    private int legs;
+    private String material;
+
+    public Chair() {
+        this.legs = 4;
+        this.material = "wood";
+    }
+
+    public Chair(int legs) {
+        this.legs = legs;
+        this.material = "wood";
+    }
+
+    public Chair(int legs, String material) {
+        this.legs = legs;
+        this.material = material;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
 }
